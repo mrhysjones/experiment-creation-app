e2e5798725f53bdecc32d0b65cb9597ba4df4414
Matthew Jones <contact@matt-j.me>
2016-03-15 18:18:55 +0000

    app.use CORS error

--- webapp/app.js
+++ webapp/app.js
@@ -30,7 +30,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json({limit: '1mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('cors');
+app.use(cors);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/experiments', experiments);
